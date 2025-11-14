5d328b4de52c782d90baffd3c95e96f18e6cf713
Nannasarah <nannasp01@gmail.com>
2025-11-14 12:13:10 +0100

    Enhance product detail view: update image sources and display product title and description dynamically

--- src/app/(routes)/products/[id]/page.js
+++ src/app/(routes)/products/[id]/page.js
@@ -3,8 +3,25 @@ import Footer from "@/components/layout/Footer";
 import BackButton from "@/components/ui/BackButton";
 import ProductInfo from "@/components/ProductInfo";
 import Reviews from "@/components/Reviews";
+import { Suspense } from "react";
 
 export default function Product({ searchParams }) {
+  return (
+    <Suspense>
+      <Productcontainer searchParams={searchParams} />
+    </Suspense>
+  );
+}
+
+async function Productcontainer({ searchParams }) {
+  // Henter id fra URL'en — fx /detalje?id=7
+  const params = await searchParams;
+  const id = params.id;
+
+  // Henter produktet fra API'et
+  const response = await fetch(`https://dummyjson.com/products/${id}`);
+  const product = await response.json();
+
   return (
     <>
       <Navigation />
--- src/components/cards/ProductView.jsx
+++ src/components/cards/ProductView.jsx
@@ -6,8 +6,8 @@ const ProductView = () => {
       <div className="grid gap-3">
         <Image
           loading="eager"
-          alt="gobibjoern.png"
-          src="/gobibjoern.png"
+          alt={product.title}
+          src={product.thumbnail}
           width={400}
           height={200}
           className="rounded-2xl"
@@ -15,24 +15,24 @@ const ProductView = () => {
         <div className="grid grid-cols-3 gap-3">
           <Image
             loading="eager"
-            alt="gobibjoern.png"
-            src="/gobibjoern.png"
+            alt={product.title}
+            src={product.thumbnail}
             width={300}
             height={200}
             className="rounded-2xl"
           />
           <Image
             loading="eager"
-            alt="gobibjoern.png"
-            src="/gobibjoern.png"
+            alt={product.title}
+            src={product.thumbnail}
             width={300}
             height={200}
             className="rounded-2xl"
           />
           <Image
             loading="eager"
-            alt="gobibjoern.png"
-            src="/gobibjoern.png"
+            alt={product.title}
+            src={product.thumbnail}
             width={300}
             height={200}
             className="rounded-2xl"
@@ -40,13 +40,8 @@ const ProductView = () => {
         </div>
       </div>
       <div>
-        <h2>Gobi</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
-          soluta consectetur provident quam, quos hic obcaecati, laboriosam
-          repellendus ipsam veritatis ullam tempora, nemo facere voluptatum
-          culpa consequatur exercitationem praesentium corporis!
-        </p>
+        <h2>{product.title}</h2>
+        <p>{product.description}</p>
       </div>
     </section>
   );
