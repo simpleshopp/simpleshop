3c35fe7b1dc64de1eecc2a0da24353564bc25b3c
Nannasarah <nannasp01@gmail.com>
2025-11-14 10:19:22 +0100

    Add Navigation component and update package dependencies

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,7 +1,26 @@
+import { HiOutlineCake } from "react-icons/hi";
+import { SlBasket } from "react-icons/sl";
+
 const Navigation = () => {
-    return ( 
-        <h1>Hej</h1>
-     );
-}
- 
+  return (
+    <nav className="flex items-center justify-between px-26 py-6 bg-blue-200">
+      <div className="flex items-center gap-16">
+        <a href="/">
+          <HiOutlineCake color="blue" size="60" />
+        </a>
+
+        <ul className="flex gap-6">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+        </ul>
+      </div>
+      <SlBasket size="25" cursor="pointer" href="/payment" />
+    </nav>
+  );
+};
+
 export default Navigation;
