736a524d147537f567fef64326564b5e8a79a6c8
fridacetti <frjo0003@stud.kea.dk>
2025-11-14 11:52:15 +0100

    Refactor product and payment components; enhance layout and styling

--- src/components/cards/PaymentCard.jsx
+++ src/components/cards/PaymentCard.jsx
@@ -1,5 +1,40 @@
-const PaymentCard= () => {
-  return( <></> );
+import Link from "next/link";
+
+import PaymentButton from "../ui/PaymentButton";
+const PaymentCard = () => {
+  return (
+    <>
+      <div className="border-2 border-button rounded-2xl max-h-fit m-3 p-10 mx-auto flex flex-col items-center gap-4">
+        <div className="w-full flex justify-between">
+          <div>
+            <p>Title</p>
+          </div>
+          <div>
+            <p>Price</p>
+          </div>
+        </div>
+        <div className="w-full flex justify-between">
+          <div>
+            <p>Title</p>
+          </div>
+          <div>
+            <p>Price</p>
+          </div>
+        </div>
+        <div className="w-full flex justify-between">
+          <div>
+            <p>Totale</p>
+          </div>
+          <div>
+            <p>Price</p>
+          </div>
+        </div>
+<Link href="/payment">
+        <PaymentButton />
+</Link>
+      </div>
+    </>
+  );
 };
 
 export default PaymentCard;
