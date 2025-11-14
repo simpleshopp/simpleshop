f470a8794d06f7ccdb546af05aa967f663b4e5f1
Nannasarah <nannasp01@gmail.com>
2025-11-14 10:45:09 +0100

    Add ProductInfo component to display product details

---
+++ src/components/ProductInfo.jsx
@@ -0,0 +1,12 @@
+import ProductView from "@/components/cards/ProductView";
+
+const ProductInfo = () => {
+  return (
+    <section className="grid">
+      <ProductView />
+      <div></div>
+    </section>
+  );
+};
+
+export default ProductInfo;
