75e2135a7bd510783f62743062dc5579ca53b1f4
Alberte-Remmer <alre0003@stud.kea.dk>
2025-11-14 11:52:07 +0100

    Refactor Reviews and ReviewCard components to improve structure and display reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,10 +1,19 @@
-const Reviews = () => {
-    return ( 
+import ReviewCard from "./cards/ReviewCard";
 
-        <div>
-            <h2>Reviews: </h2>
-        </div>
-     );
-}
- 
+
+const Reviews = () => {
+  return (
+    <div className=" px-26">
+      <div className="h-px w-4/5 bg-gray-300 my-4 mx-auto"></div>{" "}
+      {/* Når stregens bredde er mindre en 100% (fx. w-4/5 80%) vil mx-auto automatisk fordele den resterende plads på begge sider af elementet */}
+      <h2>Reviews: </h2>
+      <div className="flex gap-8 justify-center py-4">
+        <ReviewCard />
+        <ReviewCard />
+        <ReviewCard />
+      </div>
+    </div>
+  );
+};
+
 export default Reviews;
