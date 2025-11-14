ed0ef0a10f470fcd622578dea1cfca07632670bd
fridacetti <frjo0003@stud.kea.dk>
2025-11-14 09:43:37 +0100

    Fix import paths for ProductList, Category, and SearchBar components in Products page

--- src/app/(routes)/products/page.js
+++ src/app/(routes)/products/page.js
@@ -1,10 +1,9 @@
 import Navigation from "@/components/layout/Navigation";
 import Footer from "@/components/layout/Footer";
-import ProductList from "./[id]/page";
-import Category from "@/components/ui/Category"
-import SearchBar from "@/components/ui/SearchBar"
+import ProductList from "@/components/ProductList";
+import Category from "@/components/ui/Category";
+import SearchBar from "@/components/ui/SearchBar";
 import Payment from "../payment/page";
-
 
 export default function Products({ searchParams }) {
   return (
