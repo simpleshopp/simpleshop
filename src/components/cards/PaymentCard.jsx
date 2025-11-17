b6e541a0357eb3bdf061698fcd22760890b34ddb
Nannasarah <nannasp01@gmail.com>
2025-11-17 13:08:13 +0100

    Remove unused code and comments from PaymentCard component

--- src/components/cards/PaymentCard.jsx
+++ src/components/cards/PaymentCard.jsx
@@ -39,54 +39,6 @@ const PaymentCard = () => {
       >
         Go to Cart
       </button>
-      //{" "}
-      <div className="border-2 border-button rounded-2xl max-h-fit m-3 p-10 mx-auto flex flex-col items-center gap-4">
-        //{" "}
-        <div className="w-full flex justify-between">
-          //{" "}
-          <div>
-            // <p>Title</p>
-            //{" "}
-          </div>
-          //{" "}
-          <div>
-            // <p>Price</p>
-            //{" "}
-          </div>
-          //{" "}
-        </div>
-        //{" "}
-        <div className="w-full flex justify-between">
-          //{" "}
-          <div>
-            // <p>Title</p>
-            //{" "}
-          </div>
-          //{" "}
-          <div>
-            // <p>Price</p>
-            //{" "}
-          </div>
-          //{" "}
-        </div>
-        //{" "}
-        <div className="w-full flex justify-between">
-          //{" "}
-          <div>
-            // <p>Totale</p>
-            //{" "}
-          </div>
-          //{" "}
-          <div>
-            // <p>Price</p>
-            //{" "}
-          </div>
-          //{" "}
-        </div>
-        <Link href="/payment">
-          <PaymentButton />
-        </Link>
-      </div>
     </>
   );
 };
