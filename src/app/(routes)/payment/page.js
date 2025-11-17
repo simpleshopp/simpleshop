6dee0b1eb11d98d16352878698bf6032515617ad
Nannasarah <nannasp01@gmail.com>
2025-11-17 14:07:52 +0100

    Add BackButton component to Payment page layout

--- src/app/(routes)/payment/page.js
+++ src/app/(routes)/payment/page.js
@@ -1,11 +1,13 @@
 import Navigation from "@/components/layout/Navigation";
 import Footer from "@/components/layout/Footer";
 import CheckOutCard from "@/components/cards/CheckOutCard";
+import BackButton from "@/components/ui/BackButton";
 
 export default function Payment({ searchParams }) {
   return (
     <>
       <Navigation />
+      <BackButton />
       <div className="grid px-26">
         <CheckOutCard />
       </div>
