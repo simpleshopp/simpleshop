d819ebfe0b36145a93c7c338633587d3ad6b944a
Nannasarah <nannasp01@gmail.com>
2025-11-17 10:20:54 +0100

    Update BackButton styling for improved layout

--- src/components/ui/BackButton.jsx
+++ src/components/ui/BackButton.jsx
@@ -4,7 +4,7 @@ const BackButton = () => {
   return (
     <>
       <Link href="/products">
-        <button className="px-6 py-2 rounded-2xl mb-8 cursor-pointer">
+        <button className="px-6 py-2 rounded-2xl mb-8 cursor-pointer mx-26 mt-10">
           back
         </button>
       </Link>
